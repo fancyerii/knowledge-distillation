652de1341257baa286398dc0739ae0fd14333d78
clementcj <clementcj@126.com>
2016-12-06 15:51:37 +0800

    ok

--- fjnet_crawler/scripts/schedule.sh
+++ fjnet_crawler/scripts/schedule.sh
@@ -6,4 +6,4 @@ else
 fi
 find queuedata -type f -exec rm {} \;
 
-java -Dlog4j.configuration=log4j-sche.properties -cp ".:./fjnet_crawler-1.0-SNAPSHOT-jar-with-dependencies.jar" com.antbrains.fjnet_crawler.scheduler.SchedulerDriver --maxEntriesLocalHeap 1000000 --batchSize 1000 --maxQueueSize 50000  "jnp://ai-dev:1099" ai-dev:3333 fjnet queuedata 22345 &
+java -Dlog4j.configuration=log4j-sche.properties -cp ".:./fjnet_crawler-1.0-SNAPSHOT-jar-with-dependencies.jar" com.antbrains.fjnet_crawler.scheduler.SchedulerDriver --maxEntriesLocalHeap 1000000 --batchSize 1000 --maxQueueSize 50000  "jnp://10.26.161.20:1099" 10.26.161.20:3333 fjnet queuedata 22366 &
