506384ac1877604c61bbadb576d8b02f9f12c3ea
clementcj <clementcj@126.com>
2016-12-06 15:42:49 +0800

    add crawler rufodao

---
+++ rufodao_crawler/scripts/crawl.sh
@@ -0,0 +1,1 @@
+java  -Dlog4j.configuration=log4j-crawl.properties -cp ".:./rufodao_crawler-1.0-SNAPSHOT-jar-with-dependencies.jar" com.antbrains.rufodao.scheduler.CrawlerDriver --isUpdate true --stopPort 9115 --workerNum 10 "jnp://10.26.161.20:1099" 10.26.161.20:3333 rufodao com.antbrians.rufodao.extractor.RufodaoExtractor queuedata &
---
+++ rufodao_crawler/src/main/java/com/antbrains/rufodao/scheduler/SchedulerDriver.java
@@ -0,0 +1,12 @@
+package com.antbrains.rufodao.scheduler;
+
+import com.antbrains.mysqltool.PoolManager;
+import com.antbrains.sc.scheduler.FileUnfinishedScheduler;
+
+public class SchedulerDriver {
+
+	public static void main(String[] args) throws Exception {
+		FileUnfinishedScheduler.main(args);
+	}
+
+}
