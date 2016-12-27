ad22026b4026c37e044bc55e3c429039205a6272
clementcj <clementcj@126.com>
2016-12-27 13:36:54 +0800

    saraxwzx_es

---
+++ saraxwzx_es/src/main/java/com/antbrains/saraxwzx/tools/gson/GsonTool.java
@@ -0,0 +1,15 @@
+package com.antbrains.saraxwzx.tools.gson;
+
+import java.text.DateFormat;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonTool {
+	public static Gson getGson(){
+        GsonBuilder gb = new GsonBuilder();
+        gb.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        Gson gson = gb.create();
+        return gson;	
+	}
+}
